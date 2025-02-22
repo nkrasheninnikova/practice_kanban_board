d997210e0f8c25e85bc6a8d356114b92999b959a
Наталья Крашенинникова <gr431_krnvi@lab-305-3.tomtit-tomsk.ru>
2025-02-22 16:14:06 +0700

    data commit

--- js/main.js
+++ js/main.js
@@ -35,17 +35,26 @@ new Vue({
             this.closeForm();
         },
         deleteTask(index) {
-            this.tasks.splice(index, 1);
+            this.tasks = [...this.tasks.slice(0, index), ...this.tasks.slice(index + 1)];
         },
         moveToInProgress(index) {
-            const task = this.tasks.splice(index, 1)[0];
-            task.status = 'В работе';
-            this.inProgressTasks.push(task);
+            const task = this.tasks[index];
+            if (task) {
+                task.status = 'В работе';
+                this.inProgressTasks = [...this.inProgressTasks, task];
+                this.tasks = [...this.tasks.slice(0, index), ...this.tasks.slice(index + 1)];
+            }
         },
         startEdit(index) {
             this.editIndex = index; // Переключаем задачу в режим редактирования
         },
         saveEdit(index) {
+            if (this.editIndex !== null) {
+                const task = this.tasks[this.editIndex];
+                if (task) {
+                    task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку редактирования
+                }
+            }
             this.editIndex = null; // Выходим из режима редактирования
         },
         cancelEdit() {
@@ -60,21 +69,41 @@ new Vue({
             this.returnReason = '';
         },
         returnTaskToInProgress() {
-            const task = this.testingTasks.splice(this.returnIndex, 1)[0];
-            task.returnReason = this.returnReason;
-            task.status = 'Возвращено в работу';
-            this.inProgressTasks.push(task);
+            const task = this.testingTasks[this.returnIndex];
+            if (task) {
+                task.returnReason = this.returnReason;
+                task.status = 'Возвращено в работу';
+                task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку при возврате
+                this.inProgressTasks = [...this.inProgressTasks, task];
+                this.testingTasks = [
+                    ...this.testingTasks.slice(0, this.returnIndex),
+                    ...this.testingTasks.slice(this.returnIndex + 1)
+                ];
+            }
             this.closeReturnForm();
         },
         moveToTesting(index) {
-            const task = this.inProgressTasks.splice(index, 1)[0];
-            task.status = 'Тестирование';
-            this.testingTasks.push(task);
+            const task = this.inProgressTasks[index];
+            if (task) {
+                task.status = 'Тестирование';
+                this.testingTasks = [...this.testingTasks, task];
+                this.inProgressTasks = [
+                    ...this.inProgressTasks.slice(0, index),
+                    ...this.inProgressTasks.slice(index + 1)
+                ];
+            }
         },
         moveToCompleted(index) {
-            const task = this.testingTasks.splice(index, 1)[0];
-            task.status = 'Выполнено';
-            this.completedTasks.push(task);
+            const task = this.testingTasks[index];
+            if (task) {
+                task.status = 'Выполнено';
+                task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку при завершении
+                this.completedTasks = [...this.completedTasks, task];
+                this.testingTasks = [
+                    ...this.testingTasks.slice(0, index),
+                    ...this.testingTasks.slice(index + 1)
+                ];
+            }
         }
     }
 });
