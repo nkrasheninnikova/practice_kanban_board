5ac818b3551e92276623080856ba48760137f5ff
Natasha Krasheninnikova <nk110905@mail.ru>
2025-02-24 14:02:16 +0700

    dragn drop commit

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@ new Vue({
         showForm: false,
         showReturnForm: false,
         returnReason: '',
-        editIndex: null, // Индекс задачи, которая сейчас редактируется
+        editIndex: null,
+        currentArray: null,
+        draggedTask: null,
         newTask: {
             title: '',
             description: '',
             deadline: ''
         },
-        tasks: [], // Запланированные задачи
+        tasks: [], // Задачи
+        plannedTasks: [], // Запланированные задачи
         inProgressTasks: [], // Задачи в работе
         testingTasks: [], // Тестирование
         completedTasks: [] // Выполненные задачи
@@ -29,45 +32,31 @@ new Vue({
                 description: this.newTask.description,
                 deadline: this.newTask.deadline,
                 createdAt: new Date().toLocaleString(),
-                lastEditedAt: null, // Добавляем поле lastEditedAt и инициализируем его как null
-                status: 'Запланировано'
+                lastEditedAt: null
             };
             this.tasks.push(newTask);
             this.closeForm();
         },
-        deleteTask(index) {
-            // Удаляем задачу из массива tasks
-            this.tasks.splice(index, 1);
+        deleteTask(index, array) {
+            array.splice(index, 1);
         },
-        moveToInProgress(index) {
-            const task = this.tasks[index];
-            if (task) {
-                task.status = 'В работе';
-                this.inProgressTasks.push(task); // Добавляем задачу в inProgressTasks
-                this.tasks.splice(index, 1); // Удаляем задачу из tasks
-            }
+        startEdit(index, array) {
+            this.editIndex = index;
+            this.currentArray = array;
         },
-        startEdit(index) {
-            this.editIndex = index; // Переключаем задачу в режим редактирования
-        },
-        saveEdit(index) {
-            if (this.editIndex !== null) {
-                // Определяем, в каком массиве находится задача
-                let taskArray = this.tasks;
-                if (this.inProgressTasks[this.editIndex]) {
-                    taskArray = this.inProgressTasks;
-                } else if (this.testingTasks[this.editIndex]) {
-                    taskArray = this.testingTasks;
-                }
-                const task = taskArray[this.editIndex];
+        saveEdit() {
+            if (this.editIndex !== null && this.currentArray) {
+                const task = this.currentArray[this.editIndex];
                 if (task) {
-                    task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку редактирования
+                    task.lastEditedAt = new Date().toLocaleString();
                 }
             }
-            this.editIndex = null; // Выходим из режима редактирования
+            this.editIndex = null;
+            this.currentArray = null;
         },
         cancelEdit() {
-            this.editIndex = null; // Отменяем редактирование
+            this.editIndex = null;
+            this.currentArray = null;
         },
         openReturnForm(index) {
             this.returnIndex = index;
@@ -80,35 +69,47 @@ new Vue({
         returnTaskToInProgress() {
             const task = this.testingTasks[this.returnIndex];
             if (task) {
-                task.returnReason = this.returnReason; // Сохраняем причину возврата
+                task.returnReason = this.returnReason;
                 task.status = 'Возвращено в работу';
-                task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку при возврате
-                this.inProgressTasks.push(task); // Добавляем задачу обратно в "Задачи в работе"
-                this.testingTasks.splice(this.returnIndex, 1); // Удаляем задачу из "Тестирования"
+                task.lastEditedAt = new Date().toLocaleString();
+                this.inProgressTasks.push(task);
+                this.testingTasks.splice(this.returnIndex, 1);
             }
             this.closeReturnForm();
         },
-        moveToTesting(index) {
-            const task = this.inProgressTasks[index];
-            if (task) {
-                task.status = 'Тестирование';
-                this.testingTasks.push(task); // Добавляем задачу в testingTasks
-                this.inProgressTasks.splice(index, 1); // Удаляем задачу из inProgressTasks
-            }
+        startDrag(event, task) {
+            this.draggedTask = task;
+            event.dataTransfer.setData('text/plain', JSON.stringify(task));
         },
-        moveToCompleted(index) {
-            const task = this.testingTasks[index];
-            if (task) {
-                const deadlineDate = new Date(task.deadline);
-                const currentDate = new Date();
-                if (currentDate > deadlineDate) {
-                    task.status = 'Просрочено';
-                } else {
-                    task.status = 'Выполнено';
-                }
-                task.lastEditedAt = new Date().toLocaleString(); // Добавляем временную метку при завершении
-                this.completedTasks.push(task); // Добавляем задачу в completedTasks
-                this.testingTasks.splice(index, 1); // Удаляем задачу из testingTasks
+        onDrop(event, targetArrayName) {
+            const taskData = JSON.parse(event.dataTransfer.getData('text/plain'));
+            const task = this.draggedTask;
+
+            // Удаляем задачу из текущего массива
+            if (this.tasks.includes(task)) {
+                this.tasks.splice(this.tasks.indexOf(task), 1);
+            } else if (this.plannedTasks.includes(task)) {
+                this.plannedTasks.splice(this.plannedTasks.indexOf(task), 1);
+            } else if (this.inProgressTasks.includes(task)) {
+                this.inProgressTasks.splice(this.inProgressTasks.indexOf(task), 1);
+            } else if (this.testingTasks.includes(task)) {
+                this.testingTasks.splice(this.testingTasks.indexOf(task), 1);
+            } else if (this.completedTasks.includes(task)) {
+                this.completedTasks.splice(this.completedTasks.indexOf(task), 1);
+            }
+
+            // Добавляем задачу в целевой массив
+            if (targetArrayName === 'tasks') {
+                this.tasks.push(task);
+            } else if (targetArrayName === 'plannedTasks') {
+                this.plannedTasks.push(task);
+            } else if (targetArrayName === 'inProgressTasks') {
+                this.inProgressTasks.push(task);
+            } else if (targetArrayName === 'testingTasks') {
+                this.testingTasks.push(task);
+            } else if (targetArrayName === 'completedTasks') {
+                this.completedTasks.push(task);
+                task.status = 'Выполнено';
             }
         }
     }
